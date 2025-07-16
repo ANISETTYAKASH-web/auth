da254fc88fe0b018d7958f0be21cdb410c4f2904
akash <akashanisetty7@gmail.com>
2025-07-16 16:53:59 +0100

    Added resendOtp and resetPassword Functions fixed small bug

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import connect from "./config/mangoDB.js";
+import authRouter from "./routes/authRoutes.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
 connect();
+
+//API ENDPOINTS
 app.get("/", (req, res) => {
   res.send("hello world // API WORKING");
 });
+app.use("/api/auth", authRouter);
 app.listen(process.env.PORT, () => {
   console.log(`listening on port:${process.env.PORT}`);
 });
