724cdcebc8b6e21024fbd08b1c279e699eaeb39a
akash <akashanisetty7@gmail.com>
2025-07-16 16:56:46 +0100

    Included cookie parser to parse cookies

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ import "dotenv/config";
 import cors from "cors";
 import connect from "./config/mangoDB.js";
 import authRouter from "./routes/authRoutes.js";
+import cookieParser from "cookie-parser";
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 connect();
 
 //API ENDPOINTS
