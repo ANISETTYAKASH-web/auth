688de8c4412550f8c4d9a24306eaf73ed8004e7d
akash <akashanisetty7@gmail.com>
2025-07-16 16:55:50 +0100

    Added multiple routes mainly including otps

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,7 +1,20 @@
 import express from "express";
-import { loginUser, logout, register } from "../controller/userController.js";
+import {
+  loginUser,
+  logout,
+  register,
+  resendOtp,
+  resetPassword,
+  sendOtp,
+  verifyOtp,
+} from "../controller/userController.js";
+import getUserId from "../middleware/authToken.js";
 const authRouter = express.Router();
 authRouter.post("/register", register);
 authRouter.post("/login", loginUser);
 authRouter.post("/logout", logout);
+authRouter.post("/send-otp", getUserId, sendOtp);
+authRouter.post("/verify-otp", getUserId, verifyOtp);
+authRouter.post("/resend-otp", resendOtp);
+authRouter.post("/reset-password", resetPassword);
 export default authRouter;
