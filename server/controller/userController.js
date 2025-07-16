2f3c4d3c4be9e93b5916fb65f07ccfc24d2c9dcb
akash <akashanisetty7@gmail.com>
2025-07-16 07:53:29 +0100

    feat(userController): wrote user login function

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -24,6 +24,38 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "null" : "Strict",
       maxAge: 86400000, //86400000 is 24 hrs in milliseconds
     });
+    return res.json({ success: true });
+  } catch (error) {
+    res.send({ success: false, error: error.message });
+  }
+};
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.send({ success: false, message: "Details missing" });
+  }
+  try {
+    const user = await userModel.findOne({ user });
+    if (!user) {
+      return res.send({
+        success: false,
+        message: "user with email doesn't exist",
+      });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.send({ success: false, message: "Incorrect Password" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "null" : "Strict",
+      maxAge: 86400000, //86400000 is 24 hrs in milliseconds
+    });
+    return res.json({ success: true });
   } catch (error) {
     res.send({ success: false, error: error.message });
   }
